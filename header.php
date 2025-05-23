89706773cd479d3544632a4f8634e8648b52538e
StrykerUX <abraham.stryker117@gmail.com>
2025-05-23 05:58:16 -0600

    Test Display Alias on Dashboard

--- header.php
+++ header.php
@@ -146,8 +146,8 @@ $is_default_template = is_page_template('page-templates/default.php');
                             ?>
                         </div>
                         <div class="user-info">
-                            <div class="user-name"><?php echo esc_html($current_user->display_alias); ?></div>
-                            <div class="user-plan"><?php _e('Free plan', 'nova-template-wp-solar'); ?></div>
+                            <div class="user-name"><?php echo esc_html($current_user->display_name); ?></div>
+                            <div class="user-plan"><?php _e('Beta plan', 'nova-template-wp-solar'); ?></div>
                         </div>
                     </div>
                     <!-- Action Button -->
